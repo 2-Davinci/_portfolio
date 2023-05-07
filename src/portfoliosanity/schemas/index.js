d1190f089b330fbebc7736bd0d43cd0d1a707ae9
2-Davinci <116849278+2-Davinci@users.noreply.github.com>
2023-05-07 14:21:12 +0000

    init

---
+++ src/portfoliosanity/schemas/index.js
@@ -0,0 +1,18 @@
+import about from './about'
+import brands from './brands'
+import experience from './experience'
+import contact from './contact'
+import skills from './skills'
+import testimonial from './testimonial'
+import work from './work'
+import workExperience from './workExperience'
+export const schemaTypes = [
+  about,
+  brands,
+  experience,
+  contact,
+  skills,
+  testimonial,
+  work,
+  workExperience,
+]
